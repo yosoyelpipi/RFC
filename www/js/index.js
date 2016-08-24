cf843bc018232744d4b5d3c4f0c562db49e701d6
Leo Condori <lcondori@gmail.com>
2016-08-24 00:00:27 -0300

    1.0

--- www/js/index.js
+++ www/js/index.js
@@ -7,24 +7,26 @@ var app = {
     },
     onDeviceReady: function() {
         nfc.addNdefListener(app.onNfc);
+        alert('Arranque');
     },
     onNfc: function(nfcEvent) {
         var tag = nfcEvent.tag,
             message = tag.ndefMessage,
             record = message[0],
             value;
+            alert('pase1');
     
         if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_URI)) {    
             value = ndef.uriHelper.decodePayload(record.payload);
-                            
+            alert('pase2');                
         } else if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_TEXT)) {
             value = ndef.textHelper.decodePayload(record.payload);    
-            
+            alert('pase3');
         } else {
             value = JSON.stringify(record);    
-            
+            alert('pase4');
         }
-    
+        alert('pase5');
         alert(value);
     
     }
